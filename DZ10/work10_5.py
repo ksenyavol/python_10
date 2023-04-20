f55f36ed3bb28db393e3d80e52ef0e13a990253c
ksenyavol <120124961+ksenyavol@users.noreply.github.com>
2023-04-20 20:18:17 +0300

    Update work10_5.py

--- DZ10/work10_5.py
+++ DZ10/work10_5.py
@@ -1,36 +1,22 @@
-# 5.	Выполнить пинг веб-ресурсов yandex.ru, youtube.com и
+# 5.Выполнить пинг веб-ресурсов yandex.ru, youtube.com и
 # преобразовать результаты из байтовового в строковый тип на кириллице.
 
+
 import subprocess
 import chardet
 import os
 
-results = ''
 ARGS = ['ping', 'yandex.ru']
-subprocess_ping = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-for line in subprocess_ping.stdout:
-    results += line.decode('cp866')
+YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+for line in YA_PING.stdout:
+    res = chardet.detect(line)
+    print(line.decode(encoding=res['encoding']))
 
 ARGS = ['ping', 'youtube.com']
-subprocess_ping = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-for line in subprocess_ping.stdout:
-    results += line.decode('cp866')
+YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+for line in YA_PING.stdout:
+    res = chardet.detect(line)
+    print(line.decode(encoding=res['encoding']))
 
-print(results.encode('utf-8').decode('utf-8'))
+print(os.name)
 
-import subprocess
-import chardet
-import os
-
-results = ''
-ARGS = ['ping', 'youtube.com']
-subprocess_ping = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-for line in subprocess_ping.stdout:
-    results += line.decode('cp866')
-
-ARGS = ['ping', 'youtube.com']
-subprocess_ping = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-for line in subprocess_ping.stdout:
-    results += line.decode('cp866')
-
-print(results.encode('utf-8').decode('utf-8'))
